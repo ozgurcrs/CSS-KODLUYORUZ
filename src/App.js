84df6b209f46307b29585a7b3fc6672e4013f854
Özgür Çiriş <ozgurciris@gmail.com>
2021-03-17 03:02:56 +0300

    initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content">
+        <header>
+          <div className="userInfo">
+            <i class="fas fa-apple-alt"></i>
+            <label>Hi! Özgür Çiriş</label>
+          </div>
+          <div className="basket">
+            <div className="shopping">
+              <label>1</label>
+              <i class="fas fa-shopping-basket"></i>
+              
+            </div>
+          </div>
+        </header>
+       <div className="mainContainer">
+            <div className="cards">
+              <div className="card animate__animated animate__tada">
+                <img src="https://purepng.com/public/uploads/large/purepng.com-lays-potato-chipsfood-potato-tasty-pack-lays-chips-taste-product-snaks-9415246340529nj3f.png" alt=""/>
+              </div>
+            </div>
+       </div>
+      </div>
     </div>
   );
 }
